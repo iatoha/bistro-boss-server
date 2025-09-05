0b9432fab7ab4866a4882a1f33864363c0b27e74
iatoha <iatohaph@gmail.com>
2025-09-05 11:53:23 +0600

    user panel loading

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ async function run() {
     const cartCollection = client.db("bistroDb").collection("carts");
 
     // user related api
+    app.get('/users', async(req, res) =>{
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
     app.post('/users', async (req, res) => {
       const user = req.body;
@@ -43,11 +47,18 @@ async function run() {
       if(existingUser){
         return res.send({ message: 'user already exists', insertedId: null })
       }
-
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
 
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // menu related api
     app.get('/menu', async (req, res) =>{
       const result = await menuCollection.find().toArray();
       res.send(result);
