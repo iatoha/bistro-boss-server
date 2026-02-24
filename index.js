8adbcc2a2c421012168212019b3ef11d2c169102
iatoha <iatohaph@gmail.com>
2026-02-24 23:12:53 +0600

    all done backend

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ie1t17w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ie1t17w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
